3b649f882cd7c232e811e62de7adf765488ab5c5
meshrrr <volframhome@gmail.com>
2025-05-11 17:45:03 +0500

    add database + main.py with app

--- university-jobs/backend/main.py
+++ university-jobs/backend/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from schemas import VacancyCreate
+from models import Base, User, Vacancy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 app = FastAPI()
 
-@app.get("/vacancies")
+engine = create_engine(os.getenv("DATABASE_URL"))
+Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def home():
+    return {"message": "Hello!"}
