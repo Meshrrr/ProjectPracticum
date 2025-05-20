51bc3fec3fd506838223c2b5bf6dabe74678178b
meshrrr <volframhome@gmail.com>
2025-05-20 22:22:06 +0500

    +endpoints and restruct file

--- university-jobs/backend/main.py
+++ university-jobs/backend/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI, HTTPException
-from schemas import VacancyCreate
-from models import Base, User, Vacancy
+from fastapi.params import Depends
+
+from schemas import VacancyCreate, ApplicationCreate
+from models import User, Vacancy, Application
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import declarative_base, sessionmaker, Session
 import os
 from dotenv import load_dotenv
+
+Base = declarative_base()
+
 
 load_dotenv()
 
@@ -12,8 +17,34 @@ load_dotenv()
 app = FastAPI()
 
 engine = create_engine(os.getenv("DATABASE_URL"))
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base.metadata.create_all(bind=engine)
 
-@app.get("/")
-def home():
-    return {"message": "Hello!"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/vacancies/")
+def create_vacancy(vacancy: VacancyCreate, db: Session = Depends(get_db)):
+    db_vacancy = Vacancy(**vacancy.dict())
+    db.add(db_vacancy)
+    db.commit()
+    return db_vacancy
+
+@app.get("/vacancies/")
+def read_vacancies(department: str = None, db: Session = Depends(get_db)):
+    query = db.query(Vacancy)
+    if department:
+        query = query.filter(Vacancy.department == department)
+    return query.all()
+
+@app.post("/applications/")
+def create_application(application: ApplicationCreate, db: Session = Depends(get_db)):
+    db_application = Application(**application.dict())
+    db.add(db_application)
+    db.commit()
+    return {"message": "Заявка отправлена!"}
+
